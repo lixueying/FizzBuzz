4642f7c3a44f9b96ab96034ba0f324d2f27cf796
lixueying <lixueying23@vip.qq.com>
2020-01-10 17:24:25 +0800

    should return fizz when number is multiple of 3

---
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -0,0 +1,14 @@
+package com.thoughtworks;
+
+class FizzBuzz {
+
+    String formatFizzBuzz(int number) {
+
+//        if (number % 5 == 0) {
+//            return "Buzz";
+//        } else if (number % 7 == 0) {
+//            return "Whizz";
+//        }
+        return "Fizz";
+    }
+}
---
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -0,0 +1,43 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    public void should_return_fizz_when_number_is_multiple_of_3() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String actual = fizzBuzz.formatFizzBuzz(6);
+
+        //then
+        assertEquals(actual,"Fizz");
+    }
+
+//    @Test
+//    public void should_return_buzz_when_number_is_multiple_of_5() {
+//        //given
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+//
+//        //when
+//        String actual = fizzBuzz.formatFizzBuzz(10);
+//
+//        //then
+//        assertEquals(actual,"Buzz");
+//    }
+//
+//    @Test
+//    public void should_return_whizz_when_number_is_multiple_of_7() {
+//        //given
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+//
+//        //when
+//        String actual = fizzBuzz.formatFizzBuzz(14);
+//
+//        //then
+//        assertEquals(actual,"Whizz");
+//    }
+}
